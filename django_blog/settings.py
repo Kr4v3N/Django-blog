da5d24a7dad171b0f4852eaa6545359ab49d7ba1
Kr4v3n <fafachen37@gmail.com>
2020-01-05 17:36:23 +0100

    Added static-res folder

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -107,8 +107,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static-res'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 try:
     from django_blog.local_settings import *
