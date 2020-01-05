ccc1b5b6810af09ea51ecf6e241aaadbcbd1234b
Kr4v3n <fafachen37@gmail.com>
2020-01-05 03:20:58 +0100

    Added display of item categories

--- pyblog/views.py
+++ pyblog/views.py
@@ -7,9 +7,12 @@ from pyblog import model_helpers
 # Create your views here.
 def post_list(request, category_name=model_helpers.post_category_all.slug()):
     category, posts = model_helpers.getCategory_and_posts(category_name)
+    categories = model_helpers.get_categories()
 
     context = {
         'category': category,
         'posts': posts,
+        'categories': categories,
     }
+
     return render(request, 'blog/post_list.html', context)
