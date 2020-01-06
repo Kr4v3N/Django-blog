7917d01a5eb751b7417f56f4a64700ac26ed6d11
Kr4v3n <fafachen37@gmail.com>
2020-01-06 20:23:34 +0100

    Adding index page

--- pyblog/urls.py
+++ pyblog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('posts/', views.post_list, name='home'),
     path('posts/<str:category_name>/', views.post_list, name='post-list'),
     path('posts/detail/<int:post_id>/', views.post_detail, name='post-detail'),
+    path('', views.post_list, name='index')
 ]
